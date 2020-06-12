74cfbc8b8a33913c640494a0142fc75a0a15b640
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-06-12 12:53:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Celcius to farenheit
+function convertToFarenheit(celsius) {
+    let fahrenheit = celsius * 9/5 + 32
+    return fahrenheit;
+  }
+  
+  convertToFarenheit(88);
+
+//Farenheit to celcius
+function convertToCelcius(farenheit) {
+    let celcius = farenheit * 9/5 + 32
+    return celcius;
+  }
+  
+  convertToCelcius(11);
